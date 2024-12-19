974b0585ff6a38859867b873946dc8dd806cce38
Chiara <chiara-zuc@libero.it>
2024-12-19 22:37:08 +0100

    added config translation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    TranslateModule
+  ],
   template: '<router-outlet></router-outlet>'
 })
+
 export class AppComponent {
-  title = 'CZ | Portfolio';
+  private langs = ['en', 'it'];
+
+  constructor(private _translateService: TranslateService) {
+    this._translateService.addLangs(this.langs);
+    this._translateService.setDefaultLang('en');
+    this._translateService.use('en');
+  }
 }
