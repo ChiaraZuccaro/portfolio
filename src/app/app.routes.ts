a3388cc066e27af0336e2828ad5eb6085f957a62
Chiara <chiara-zuc@libero.it>
2024-12-24 17:49:47 +0100

    added jobs and contacts pages/routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,21 +1,17 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { LangGuard } from './guards/lang.guard';
+import { ContactsComponent } from './pages/contacts/contacts.component';
+import { JobsComponent } from './pages/jobs/jobs.component';
 
 export const routes: Routes = [
-    { 
+    {
         path: ':lang',
         canActivate: [LangGuard],
         children: [
-            {
-                path: 'home',
-                title: 'CZ | Portfolio',
-                component: HomeComponent
-            },
-            // { path: ':contacts'},
-            // { path: ':skills'},
-            // { path: ':certifications'},
-            // { path: ':jobs'},
+            { path: 'home', title: 'CZ | Portfolio', component: HomeComponent },
+            { path: 'contacts', title: 'CZ | Contacts', component: ContactsComponent },
+            { path: 'exp', title: 'CZ | Experiences', component: JobsComponent },
             { path: '**', redirectTo: 'home', pathMatch: 'full' }
         ]
     },
