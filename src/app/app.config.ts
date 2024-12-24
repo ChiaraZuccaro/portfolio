8806e1bb27e1c3599f07a78c6ca460da0eda16ae
Chiara <chiara-zuc@libero.it>
2024-12-24 12:32:13 +0100

    deleted useless import

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,27 +5,15 @@ import { routes } from './app.routes';
 import { provideHttpClient } from "@angular/common/http";
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { HttpClient } from '@angular/common/http';
-import { provideAngularSvgIcon, SvgLoader } from 'angular-svg-icon';
 
 const httpLoaderFactory: (http: HttpClient) => TranslateHttpLoader = (http: HttpClient) =>
   new TranslateHttpLoader(http, './i18n/', '.json');
-
-// export function svgLoaderFactory(http: HttpClient, transferState: TransferState) {
-//   return new SvgServerLoader('./assets/lang_ico', transferState);
-// }
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(),
-    // provideAngularSvgIcon({
-    //   loader: {
-    //     provide: SvgLoader,
-    //     useFactory: svgLoaderFactory,
-    //     deps: [ HttpClient, TransferState ],
-    //   }
-    // }),
     provideTranslateService({
       loader: {
         provide: TranslateLoader,
