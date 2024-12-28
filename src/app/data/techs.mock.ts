3f0c25e661ed7cf945b3056fa4e4c6316592d7d1
Chiara <chiara-zuc@libero.it>
2024-12-28 21:51:10 +0100

    added technologies icons

---
+++ src/app/data/techs.mock.ts
@@ -0,0 +1,24 @@
+import { Techs } from "@app/interfaces/techs.interface";
+
+export const Technologies: Techs[] = [
+  {
+    name: 'Angular (JS/v.2+)',
+    icon: './techs_ico/angular_icon.svg'
+  },
+  {
+    name: 'Typescript',
+    icon: './techs_ico/typescript_icon.svg'
+  },
+  {
+    name: 'Sass/Scss',
+    icon: './techs_ico/sass_icon.svg'
+  },
+  {
+    name: 'HTML5',
+    icon: './techs_ico/html5.svg'
+  },
+  {
+    name: 'Bootstrap (v.5+)',
+    icon: './techs_ico/bootstrap_icon.svg'
+  }
+]
