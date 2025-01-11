33b5b7829b0bc72b78e3ef428763149c4a4d1e6a
Chiara <chiara-zuc@libero.it>
2025-01-11 21:48:28 +0100

    prov sunlight set

--- src/app/classes/sunlight.class.ts
+++ src/app/classes/sunlight.class.ts
@@ -6,32 +6,31 @@ export class Sunlight {
   // 0xffce52
   private sunSettings = {
     color: 0xffa500,
-    intensity: 1,
+    intensity: 10,
     position: { x: 0, y: this.roadYLevel, z: 0 }
   }
 
-  private sunLight: DirectionalLight;
+  private sunlight: DirectionalLight;
 
   constructor(scene: Scene) {
     this.setLights();
-    scene.add(this.sunLight);
+    scene.add(this.sunlight);
   }
 
   private setLights() {
     // Create the directional light
-    this.sunLight = new DirectionalLight(this.sunSettings.color, this.sunSettings.intensity);
-    this.sunLight.castShadow = true; // Enable shadows if needed
+    this.sunlight = new DirectionalLight(this.sunSettings.color, this.sunSettings.intensity);
+    this.sunlight.castShadow = true; // Enable shadows if needed
     // Adjust shadow properties (optional)
-    this.sunLight.shadow.mapSize.width = 2048;
-    this.sunLight.shadow.mapSize.height = 2048;
-    this.sunLight.shadow.camera.near = 1;
-    this.sunLight.shadow.camera.far = 500;
+    this.sunlight.shadow.mapSize.width = 2048;
+    this.sunlight.shadow.mapSize.height = 2048;
+    this.sunlight.shadow.camera.near = 1;
+    this.sunlight.shadow.camera.far = 500;
   }
 
   public update(camera: Camera) {
     // Get the current camera position
     const cameraPosition = camera.position;
-
     // Offset the sunlight behind the camera
     const sunDistance = 50; // Distance of the sun behind the camera
     const sunHeight = 10; // Height of the sun
@@ -39,12 +38,10 @@ export class Sunlight {
     const sunX = cameraPosition.x - sunDistance * Math.cos(Math.PI / 4); // Offset behind camera
     const sunZ = cameraPosition.z - sunDistance * Math.sin(Math.PI / 4); // Offset behind camera
     const sunY = cameraPosition.y + sunHeight; // Height of the sun
-
     // Update the sunlight position
-    this.sunLight.position.set(sunX, sunY, sunZ);
-
-    // // Point the sunlight towards the center of the road
-    this.sunLight.target.position.set(0, this.roadYLevel, 0);
-    this.sunLight.target.updateMatrixWorld();
+    this.sunlight.position.set(sunX, sunY, sunZ);
+    // Point the sunlight towards the center of the road
+    this.sunlight.target.position.set(0, this.roadYLevel, 0);
+    this.sunlight.target.updateMatrixWorld();
   };
 }
